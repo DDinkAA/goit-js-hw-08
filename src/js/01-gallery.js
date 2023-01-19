36b05edc9dcde3fc126c56e1d05ebb7ec5a1269b
DDinkAA <qwertdinka@gmail.com>
2023-01-19 23:09:53 +0200

    change-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryBoard = document.querySelector('.gallery');
+
+const imagesMarkup = createGalleryMarkup(galleryItems);
+
+galleryBoard.insertAdjacentHTML('beforeend', imagesMarkup);
+
+galleryBoard.addEventListener('click', onGalleryItemClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+    </a>
+        `;
+    })
+    .join('');
+}
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
