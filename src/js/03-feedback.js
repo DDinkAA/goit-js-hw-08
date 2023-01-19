8d609957014ace1db3cca6cd320c47628c84cb59
DDinkAA <qwertdinka@gmail.com>
2023-01-19 23:25:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle";
 
+const formEl = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input");
+const messageInput = document.querySelector("textarea");
+
+formEl.addEventListener("input", throttle(dataFormEl, 500));
+formEl.addEventListener("submit", onFormSubmit);
+
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+auditLocalStorage();
+
+function dataFormEl(evt) {
+  formData[evt.target.name] = evt.target.value;
+  //   console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(formData);
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {};
+}
+
+function auditLocalStorage() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+
+  if (parsedFormData) {
+    emailInput.value = formData.email || "";
+    messageInput.value = formData.message || "";
+  }
+}
