7fd96c86783a2f96846ed9b96cf992fd4175d072
DDinkAA <qwertdinka@gmail.com>
2023-01-20 13:26:51 +0200

    03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,21 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const CURRENT_TIME = "videoplayer-current-time";
+const CURRENT_TIME = 'videoplayer-current-time';
 
-const iframe = document.getElementById("vimeo-player");
+const iframe = document.getElementById('vimeo-player');
 const player = new Player(iframe);
 
-player.on("timeupdate", throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay({ seconds }) {
   localStorage.setItem(CURRENT_TIME, seconds);
 }
 
-setCurrentTime()
-function setCurrentTime(){
-  if(!localStorage.getItem(CURRENT_TIME)){
-    return
+setCurrentTime();
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIME)) {
+    return;
   }
   player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
-
 }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,13 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector(".feedback-form");
-const emailInput = document.querySelector("input");
-const messageInput = document.querySelector("textarea");
+const formEl = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
 
-formEl.addEventListener("input", throttle(dataFormEl, 500));
-formEl.addEventListener("submit", onFormSubmit);
+formEl.addEventListener('input', throttle(dataFormEl, 500));
+formEl.addEventListener('submit', onFormSubmit);
 
-const STORAGE_KEY = "feedback-form-state";
+const STORAGE_KEY = 'feedback-form-state';
 
 let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
@@ -22,6 +22,11 @@ function dataFormEl(e) {
 function onFormSubmit(e) {
   e.preventDefault();
   // console.log(formData);
+
+  if (!emailInput.value || !messageInput.value) {
+    alert('Please make sure all fields are filled in correctly.');
+  }
+
   e.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
   formData = {};
@@ -32,7 +37,7 @@ function checkLocalStorage() {
   const parsedFormData = JSON.parse(savedFormData);
 
   if (parsedFormData) {
-    emailInput.value = formData.email || "";
-    messageInput.value = formData.message || "";
+    emailInput.value = formData.email || ' ';
+    messageInput.value = formData.message || ' ';
   }
 }
